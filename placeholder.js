501f834ded6f674402ea346644598fd2541f3f11
Richard Scoones <richardscoones@gmail.com>
2014-02-14 00:29:49 +0000

    initial commit

---
+++ placeholder.js
@@ -0,0 +1,50 @@
+﻿function placeholder(placeholder, connectedId) {
+    if (typeof connectedId === "string") {
+        connectedId = document.getElementById(connectedId);
+    }
+    this.connectedId = connectedId;
+    this.placeholder = placeholder;
+
+    function _createPlaceholder() {
+        var placeholderSupport = ("placeholder" in connectedId);
+        if (connectedId && placeholder !== "") {
+            if (placeholderSupport) {
+                _setupCSS3Placeholder();
+            } else {
+                _setupNonCSS3Placeholder();
+            }
+        }
+    }
+
+    function _setupCSS3Placeholder() {
+        connectedId.setAttribute("placeholder", placeholder);
+    }
+
+    function _setupNonCSS3Placeholder() {
+        connectedId.onblur = function () {
+            _applyWatermark();
+        }
+
+        connectedId.onfocus = function () {
+            _removeWatermark();
+        }
+
+        _applyWatermark();
+    }
+
+    function _applyWatermark() {
+        if (connectedId.value.replace(/ /g, "") === "") {
+            connectedId.value = placeholder;
+            connectedId.style.color = "#888";
+        }
+    }
+
+    function _removeWatermark() {
+        if (connectedId.value === placeholder) {
+            connectedId.value = "";
+            connectedId.style.color = "";
+        }
+    }
+    
+    _createPlaceholder();
+};
